892f80d7c84f53ec56d8691c8dabf23991748bb7
Elliot Snow-Kropla <elliot.snow-kropla@joinliora.com>
2026-03-20 14:51:26 -0300

    Add checksums

--- checksum-Elixir.ExGdal.Native.exs
+++ checksum-Elixir.ExGdal.Native.exs
@@ -1,5 +1,5 @@
 %{
-  "libex_gdal_nif-v0.5.3-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:6fdd7e1b101fb84c6bc4bc9d4ff553d68b7d93c8b4004f29b5667ee503577b78",
-  "libex_gdal_nif-v0.5.3-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:7b934e48b9b3dd4d7aa6f44bf0d8fd32fded4f79712aab052b56a85f3983ddce",
-  "libex_gdal_nif-v0.5.3-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:6214f5ea0fb43059dd989e0b0f00e2062f3367725b6cbed083bf7c7d356dbc60",
+  "libex_gdal_nif-v0.6.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:96c047938e6834df873ce59b3f5cf481abf1750161dda44c479c3b3ca137b891",
+  "libex_gdal_nif-v0.6.0-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:dcc9f8b048a8790f538f77997c18486b3f7fad498d8003088846ee4387064e11",
+  "libex_gdal_nif-v0.6.0-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b56a4194eae5c7b15577855caf38ced5d026a36bb8fae7e13ceb91a663d425d0",
 }
