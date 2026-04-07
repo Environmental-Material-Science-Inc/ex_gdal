38c23d32e1a6f9ac2013154058d132fa6b914f37
Elliot Snow-Kropla <elliot.snow-kropla@joinliora.com>
2026-04-07 10:11:53 -0300

    Don't build aarch64 musl

--- checksum-Elixir.ExGdal.Native.exs
+++ checksum-Elixir.ExGdal.Native.exs
@@ -1,7 +1,6 @@
 %{
   "libex_gdal_nif-v0.7.4-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:79b95c4cb723220d2ec3b58865d04ea924b5ab2e423156d30b07cadf729dcbff",
   "libex_gdal_nif-v0.7.4-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f2e93985d665f77db743f4523b52b5191ca1e384eae13d18619ea656c680366e",
-  "libex_gdal_nif-v0.7.4-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:TODO",
   "libex_gdal_nif-v0.7.4-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:79536ad0bdd587d3e108af8dde87ebe700ca45ac0a3341a3562850204fa9e119",
   "libex_gdal_nif-v0.7.4-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:61c8d452c50772c4cfdacab33c6aef074778bde3d85c776bb2ca5f2db3bd40e1",
   "libex_gdal_nif-v0.7.4-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:TODO",
